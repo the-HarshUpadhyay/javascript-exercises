1e61dff3bcd37495db2cb2dabac21a885e5eb32c
the-HarshUpadhyay <harsh.upadhyay.mit@gmail.com>
2026-06-18 12:22:26 +0530

    solved 09_sumaAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if(!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+    if(a < 0 || b < 0) return "ERROR";
+    let sum = 0;
+    for(let i=Math.min(a,b);i<=Math.max(a,b);i++) sum+=i;
+    return sum;
 };
 
 // Do not edit below this line
