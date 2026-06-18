b5cc674914da0760ddbac3f70cdd89693324e46a
the-HarshUpadhyay <harsh.upadhyay.mit@gmail.com>
2026-06-18 11:05:43 +0530

    Solved 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    res = "";
+    for(let i = string.length-1;i>=0;i--){
+        res += string[i];
+    }
+    return res;
 };
 
 // Do not edit below this line
