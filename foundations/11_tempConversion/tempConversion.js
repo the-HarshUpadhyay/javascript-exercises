29e9f8f5b2927e5265af60989f1e920cb0a21edb
the-HarshUpadhyay <harsh.upadhyay.mit@gmail.com>
2026-06-18 12:35:31 +0530

    11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+    return Number(((tempF-32)* 5/9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+    return Number((tempC*9/5 + 32).toFixed(1));
 };
 
 // Do not edit below this line
